d3de36ad1ad169fa56d97d29c73bc59ea1f1ca9c
Valentina Assef <valenassef@gmail.com>
2023-01-05 17:55:30 -0300

    Json, Servicios y Directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { BannerComponent } from './componentes/banner/banner.component';
